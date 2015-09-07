5f17cf33dbb6f7636ae7397c99417a9a0455f475
Richard Mikael Slevinsky <Richard.Slevinsky@maths.ox.ac.uk>
2015-09-07 12:00:54 +0100

    framework for hessian

--- src/ipoptfunctions.jl
+++ src/ipoptfunctions.jl
@@ -4,10 +4,11 @@
 function eval_f(x)
     temp1 = 0.0
     temp2 = 0.0
+    xpg = complex(x,pi/2gaopt)
     @inbounds for k=1:n
         temp3=0.0
         @inbounds for j=1:n
-            temp3+=x[n+j]*complex(x[k],pi/2gaopt)^(j-1)
+            temp3+=x[n+j]*xpg[k]^(j-1)
         end
         temp1+=ept[k]-imag(temp3)
         temp2+=cosh(x[k])*spg
@@ -20,10 +21,11 @@ function eval_g(x, g)
     # OK:  g[:] = zeros(2)  # Modifies 'in place'
     g[:] = zeros(2n)
     f = eval_f(x)
+    xpg = complex(x,pi/2gaopt)
     @inbounds for k=1:n
         temp1=0.0
         @inbounds for j=1:n
-            temp1+=x[n+j]*complex(x[k],pi/2gaopt)^(j-1)
+            temp1+=x[n+j]*xpg[k]^(j-1)
         end
         g[k] = f*sinh(x[k])*cpg + real(temp1)-dat[k]
         g[n+k] =  f*cosh(x[k])*spg + imag(temp1)-ept[k]
@@ -35,6 +37,7 @@ function eval_grad_f(x, grad_f)
     # Bad: grad_f    = zeros(4)  # Allocates new array
     # OK:  grad_f[:] = zeros(4)  # Modifies 'in place'
     grad_f[:] = zeros(2n)
+    xpg = complex(x,pi/2gaopt)
     @inbounds for r=1:n
         temp1=0.0
         temp2=0.0
@@ -43,12 +46,12 @@ function eval_grad_f(x, grad_f)
         @inbounds for k=1:n
             temp3=0.0
             @inbounds for j=1:n
-                temp3+=x[n+j]*complex(x[k],pi/2gaopt)^(j-1)
+                temp3+=x[n+j]*xpg[k]^(j-1)
             end
             temp1+=ept[k]-imag(temp3)
             temp2+=cosh(x[k])*spg
-            temp4+=x[n+k]*(k-1)*complex(x[r],pi/2gaopt)^(k-2)
-            temp5+=complex(x[k],pi/2gaopt)^(r-1)
+            temp4+=x[n+k]*(k-1)*xpg[r]^(k-2)
+            temp5+=xpg[k]^(r-1)
         end
         grad_f[r] = -(temp2*imag(temp4) + sinh(x[r])*spg*temp1)/temp2^2
         grad_f[n+r] = -imag(temp5)/temp2
@@ -68,6 +71,7 @@ function eval_jac_g(x, mode, rows, cols, values)
         values[:]=zeros(4n^2)
         f = eval_f(x)
         eval_grad_f(x,grad_f)
+        xpg = complex(x,pi/2gaopt)
         @inbounds for k=1:n
             temp1=0.0
             @inbounds for r=1:n
@@ -77,17 +81,14 @@ function eval_jac_g(x, mode, rows, cols, values)
                 values[2n^2+2n*(k-1)+r]=grad_f[r]*cosh(x[k])*spg
                 values[2n^2+2n*(k-1)+n+r]=grad_f[n+r]*cosh(x[k])*spg
 
-                values[2n*(k-1)+n+r] += real(complex(x[k],pi/2gaopt)^(r-1))
-                values[2n^2+2n*(k-1)+n+r] += imag(complex(x[k],pi/2gaopt)^(r-1))
+                values[2n*(k-1)+n+r] += real(xpg[k]^(r-1))
+                values[2n^2+2n*(k-1)+n+r] += imag(xpg[k]^(r-1))
 
-                temp1+=x[n+r]*(r-1)*complex(x[k],pi/2gaopt)^(r-2)
+                temp1+=x[n+r]*(r-1)*xpg[k]^(r-2)
             end
 
-            values[2n*(k-1)+k] += f*cosh(x[k])*cpg
-            values[2n^2+2n*(k-1)+k] += f*sinh(x[k])*spg
-
-            values[2n*(k-1)+k] += real(temp1)
-            values[2n^2+2n*(k-1)+k] += imag(temp1)
+            values[2n*(k-1)+k] += f*cosh(x[k])*cpg + real(temp1)
+            values[2n^2+2n*(k-1)+k] += f*sinh(x[k])*spg + imag(temp1)
         end
         @inbounds for k =1:n
             values[4n^2-k+1] = 0.0
@@ -99,56 +100,108 @@ function eval_jac_g(x, mode, rows, cols, values)
 end
 
 function eval_h(x, mode, rows, cols, obj_factor, lambda, values)
-    # Symmetric matrix, fill the lower left triangle only
     if mode == :Structure
-        # Again, only lower left triangle
-        # Objective
         idx = 1
-        for row = 1:2n
-            for col = 1:row
-                rows[idx] = row
-                cols[idx] = col
-                idx += 1
-            end
+        for row = 1:2n, col = 1:row
+            rows[idx] = row
+            cols[idx] = col
+            idx += 1
         end
     else
         values[:] = zeros(n*(2n+1))
         constraints = zeros(n*(2n+1))
         grad_f = zeros(2n)
         f = eval_f(x)
-        eval_grad_f(x,grad_f)  
-        @inbounds for r=1:2n
-            @inbounds for p=1:r
+        eval_grad_f(x,grad_f)
+        xpg = complex(x,pi/2gaopt)
+        for r=1:n
+            temp7=0.0 # depends on p, right? (I'm not sure.)
+
+            for p=1:r
             temp1=0.0
             temp2=0.0
             temp4=0.0
             temp5=0.0
             temp6=0.0
-            temp7=0.0
-            @inbounds   for k=1:n
-                            temp3=0.0
-                 @inbounds  for j=1:n
-                                temp3+=x[n+j]*complex(x[k],pi/2gaopt)^(j-1)
-                            end # for j
-                            temp1+=ept[k]-imag(temp3)
-                            temp2+=cosh(x[k])*spg
-                            temp4+=x[n+k]*(k-1)*complex(x[r],pi/2gaopt)^(k-2)
-                            temp5+=complex(x[k],pi/2gaopt)^(r-1)
-                            temp6+=x[n+k]*(k-1)*(k-2)*complex(x[r],pi/2gaopt)^(k-3)
-                            temp7+=x[n+k]*(k-1)*complex(x[p],pi/2gaopt)^(k-2)                
-                        end # for k
-            values[int(r*(r-1)/2+p)] =  (r<=n? ((imag(temp4)*sinh(x[p])*spg)/temp2^2 + sinh(x[r])*spg*(temp2*imag(temp7)+2*temp1*sinh(x[p])*spg)/temp2^3 - (r == p? imag(temp6)/temp2 + cosh(x[r])*spg*(temp1/temp2^2) : 0.0 )) :  (p>n? 0.0: (-temp2*imag((r-1)*complex(x[p],pi/2gaopt)^(r-2)) + sinh(x[p])*spg*imag(temp5))/temp2^2) )    
-                for kk =1:2n        
-                    if kk<=n
-                        constraints[int(r*(r-1)/2+p)] += lambda[kk]*(r<=n? values[int(r*(r-1)/2+p)]*sinh(x[kk])*cpg + (kk==p?  grad_f[r]*cosh(x[p])*cpg : 0.0) + (kk==r?  grad_f[p]*cosh(x[r])*cpg : 0.0) + (kk==r==p?  f*sinh(x[p])*cpg+real(temp6) : 0.0)  : (p>n? 0.0 : values[int((n+r)*(n+r-1)/2+p)]*sinh(x[kk])*cpg + (k==p?  grad_f[r]*cosh(x[p])*cpg + real((r-1)*complex(x[p],pi/2gaopt)^(r-2)) : 0.0) ) )
-                    elseif kk>n && kk<2n
-                        constraints[int(r*(r-1)/2+p)] += lambda[kk]*(r<=n? values[int(r*(r-1)/2+p)]*cosh(x[kk])*spg + (kk==p?  grad_f[r]*sinh(x[p])*spg : 0.0) + (kk==r?  grad_f[p]*sinh(x[r])*spg : 0.0) + (kk==r==p?  f*cosh(x[p])*spg+imag(temp6) : 0.0)  : (p>n? 0.0 : values[int((n+r)*(n+r-1)/2+p)]*cosh(x[kk])*spg + (k==p?  grad_f[r]*sinh(x[p])*spg + imag((r-1)*complex(x[p],pi/2gaopt)^(r-2)) : 0.0) ) )
-                    else
-                        constraints[int(r*(r-1)/2+p)] += 0.0    
-                    end
-                end #for kk
-                values[int(r*(r-1)/2+p)]  = obj_factor*values[int(r*(r-1)/2+p)] + constraints[int(r*(r-1)/2+p)]
-            end  # for p
+                for k=1:n
+                    temp3=0.0
+                    for j=1:n
+                        temp3+=x[n+j]*xpg[k]^(j-1)
+                    end # for j
+                    temp1+=ept[k]-imag(temp3)
+                    temp2+=cosh(x[k])*spg
+                    temp4+=x[n+k]*(k-1)*xpg[r]^(k-2)
+                    temp5+=xpg[k]^(r-1)
+                    temp6+=x[n+k]*(k-1)*(k-2)*xpg[r]^(k-3)
+                    temp7+=x[n+k]*(k-1)*xpg[p]^(k-2)
+                end # for k
+                #values[int(r*(r-1)/2+p)] =  (r<=n? ((imag(temp4)*sinh(x[p])*spg)/temp2^2 + sinh(x[r])*spg*(temp2*imag(temp7)+2*temp1*sinh(x[p])*spg)/temp2^3 - (r == p? imag(temp6)/temp2 + cosh(x[r])*spg*(temp1/temp2^2) : 0.0 )) :  (p>n? 0.0: (-temp2*imag((r-1)*xpg[p]^(r-2)) + sinh(x[p])*spg*imag(temp5))/temp2^2) )
+
+                # ∂^2 f / ∂x_r ∂ x_p
+                #values[int(r*(r-1)/2+p)] =
+
+                # ∂^2 f / ∂u_r ∂ x_p = ∂^2 f / ∂x_{n+r} ∂ x_p
+                #values[int((r+n)*(r+n-1)/2+p)] =
+            end # for p
+
+            # ∂^2 f / ∂x_r^2
+            #values[int(r*(r-1)/2+r)] =
         end # for r
+
+        for k =1:n
+            for r=1:n
+                for p=1:r
+
+                    # requires a loop for temp6
+
+                    #constraints[int(r*(r-1)/2+p)] += lambda[k]*(r<=n? values[int(r*(r-1)/2+p)]*sinh(x[kk])*cpg + (kk==p?  grad_f[r]*cosh(x[p])*cpg : 0.0) + (kk==r?  grad_f[p]*cosh(x[r])*cpg : 0.0) + (kk==r==p?  f*sinh(x[p])*cpg+real(temp6) : 0.0)  : (p>n? 0.0 : values[int((n+r)*(n+r-1)/2+p)]*sinh(x[kk])*cpg + (kk==p?  grad_f[r]*cosh(x[p])*cpg + real((r-1)*xpg[p]^(r-2)) : 0.0) ) )
+
+                    # ∂^2 g_k / ∂x_r ∂x_p
+                    #constraints[int(r*(r-1)/2+p)] += lambda[k]*
+
+                    # ∂^2 g_{n+k} / ∂x_r ∂x_p
+                    if k < n
+                        #constraints[int(r*(r-1)/2+p)] += lambda[n+k]*
+                    else
+                        #constraints[int(r*(r-1)/2+p)] += lambda[2n]*0.0
+                    end
+
+                    # ∂^2 g_k / ∂u_r ∂x_p = ∂^2 g_k / ∂x_{n+r} ∂x_p
+                    #constraints[int((n+r)*(n+r-1)/2+p)] += lambda[k]*
+
+                    # ∂^2 g_{n+k} / ∂x_{n+r} ∂x_p
+                    if k < n
+                        #constraints[int((n+r)*(n+r-1)/2+p)] += lambda[n+k]*
+                    else
+                        #constraints[int((n+r)*(n+r-1)/2+p)] += lambda[2n]*0.0
+                    end
+                end #for p
+                # ∂^2 g_k / ∂x_r ∂x_k
+                #constraints[int(r*(r-1)/2+k)] += lambda[k]*
+
+                # ∂^2 g_{n+k} / ∂x_r ∂x_k
+                if k < n
+                    #constraints[int(r*(r-1)/2+k)] += lambda[n+k]*
+                else
+                    #constraints[int(r*(r-1)/2+k)] += lambda[2n]*0.0
+                end
+
+                # ∂^2 g_k / ∂x_r ∂u_k = ∂^2 g_k / ∂x_r ∂x_{n+k}
+                #constraints[int(r*(r-1)/2+n+k)] += lambda[k]*
+
+                # ∂^2 g_{n+k} / ∂x_r ∂u_k = ∂^2 g_{n+k} / ∂x_r ∂u_{n+k}
+                if k < n
+                    #constraints[int(r*(r-1)/2+n+k)] += lambda[n+k]*
+                else
+                    #constraints[int(r*(r-1)/2+n+k)] += lambda[2n]*0.0
+                end
+            end # for r
+            # ∂^2 g_k / ∂x_k^2
+            #constraints[int(k*(k-1)/2+k)] += lambda[k]*
+            # ∂^2 g_{n+k} / ∂x_k^2
+            #constraints[int(k*(k-1)/2+k)] += lambda[n+k]*
+        end # for k
+
+        values[:] = obj_factor*values[:] + constraints[:]
     end # if loop
 end # function
